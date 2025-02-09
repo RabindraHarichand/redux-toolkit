6b7db4fa256bc582a094055b98ce764e877db50f
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-09 15:08:17 -0400

    [Axios]: Add axios for API requests and update pokemon slice with loading state management

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,3 +1,4 @@
+import { pokemonApi } from "../../../api/pokemonApi";
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice";
 
 export const getPokemons = (page = 0) => {
@@ -5,6 +6,14 @@ export const getPokemons = (page = 0) => {
     dispatch(startLoadingPokemons());
     //TODO http req
 
-    // dispatch(setPokemons())
+    // const resp = await fetch(
+    //   `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`
+    // );
+    // const data = await resp.json();
+    const { data } = await pokemonApi.get(
+      `/pokemon?limit=10&offset=${page * 10}`
+    );
+
+    dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
   };
 };
