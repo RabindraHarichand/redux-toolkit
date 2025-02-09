4af5d3b91fdd0f65d97358415b1e503ed5eefdb3
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-09 14:36:37 -0400

    [PokemonSlice]: Add PokemonApp component and integrate Pokemon slice into Redux store

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { PokemonApp } from "./PokemonApp.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PokemonApp />
     </Provider>
   </StrictMode>
 );
